eaea9618720b06d4126aa88ee8a4ed5d53595689
BESIR GUNDUZ <besirgunduz1993@gmail.com>
2021-06-03 18:11:38 +0300

    03.06.2021 Commit

--- console-programlama/Program.cs
+++ console-programlama/Program.cs
@@ -7,6 +7,13 @@ namespace console_programlama
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("İsminizi Giriniz: ");
+            var name = Console.ReadLine();
+            Console.WriteLine("Soyadınızı Giriniz: ");
+            var surname = Console.ReadLine();
+
+            Console.WriteLine($"Merhaba {name} {surname}");
+            Console.ReadKey();
         }
     }
 }
