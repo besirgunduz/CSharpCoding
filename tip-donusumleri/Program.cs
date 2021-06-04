8cb64bd766e5967f983dd611b775f3831b560ad2
BESIR GUNDUZ <besirgunduz1993@gmail.com>
2021-06-04 10:11:15 +0300

    04.06.2021 Commit

---
+++ tip-donusumleri/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace tip_donusumleri
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Implicit Conversion(Bilinçsiz Dönüşüm)
+
+            byte a = 5;
+            sbyte b = 30;
+            short c = 10;
+
+            int d = a + b + c;
+            long h = d;
+            float i = h;
+
+            string e = "Beşir";
+            char f = 'k';
+            object g = e + f + d;
+
+            Console.WriteLine(g);
+
+
+            // Explicit Convertion (Bilinçli Dönüşüm)
+
+            int x = 5;
+            byte y = (byte)x;
+            Console.WriteLine(y);
+
+            int xx = 6;
+            string yy = xx.ToString();
+
+            string zz = 12.5f.ToString();
+            Console.WriteLine(zz);
+
+            string s1 = "10";
+            int sayi1 = Convert.ToInt32(s1);
+
+            ParseMethod();
+        }
+
+        private static void ParseMethod()
+        {
+            string metin1 = "10";
+            string metin2 = "10.25";
+
+            int rakam1 = int.Parse(metin1);
+            double rakam2 = double.Parse(metin2);
+
+            string tarih = "10.10.1993";
+            var datetime = DateTime.Parse(tarih);
+            Console.WriteLine(tarih);
+
+        }
+    }
+}
