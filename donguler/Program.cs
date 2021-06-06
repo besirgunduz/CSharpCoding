d2bca36383e604c3f2761bcbc799714357297b69
Besir Gunduz <besirgunduz1993@gmail.com>
2021-06-06 16:45:22 +0300

    06.06.2021 Commit

---
+++ donguler/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace donguler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //for
+
+            int sayac = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= sayac; i++)
+            {
+                if (i % 2 == 1)
+                {
+                    continue;
+                }
+
+                Console.WriteLine(i);
+            }
+
+            int tek = 0;
+            int cift = 0;
+            for (int i = 1; i <=100; i++)
+            {
+                if (i%2==1)
+                {
+                    tek += i;
+                }
+                else
+                {
+                    cift += i;
+                }
+            }
+
+            Console.WriteLine("Tek toplam= " +tek);
+            Console.WriteLine("Çift toplam= "+cift);
+        }
+    }
+}
