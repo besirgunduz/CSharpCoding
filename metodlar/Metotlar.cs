fe9e466ea6403f197917a14fae19c66eac5fea7e
Besir Gunduz <besirgunduz1993@gmail.com>
2021-06-12 12:47:32 +0300

    12.06.2021 Commit 3

---
+++ metodlar/Metotlar.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace metodlar
+{
+    class Metotlar
+    {
+        public void EkranaYazdir(string veri)
+        {
+            Console.WriteLine(veri);
+        }
+
+        public int ArttirVeTopla(ref int deger1, ref int deger2)
+        {
+            deger1++;
+            deger2++;
+
+            return deger1 + deger2;
+        }
+
+    }
+}
---
+++ metodlar/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace metodlar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Topla(5, 10));
+            Metotlar metod = new Metotlar();
+            metod.EkranaYazdir(Topla(5, 10).ToString());
+
+            int a = 2;
+            int b = 3;
+
+            metod.EkranaYazdir(metod.ArttirVeTopla(ref a,ref b).ToString());
+            metod.EkranaYazdir(metod.ArttirVeTopla(ref a, ref b).ToString());
+
+        }
+
+        static int Topla(int sayi1, int sayi2)
+        {
+            return sayi1 + sayi2;
+        }
+
+    }
+}
