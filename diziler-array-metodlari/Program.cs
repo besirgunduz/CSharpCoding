7e038968f6ac09c6cb8dfbb9ad3256073c8b3b33
Besir Gunduz <besirgunduz1993@gmail.com>
2021-06-12 12:15:06 +0300

    12.06.2021 Commit 2

---
+++ diziler-array-metodlari/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace diziler_array_metodlari
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Sort - küçükten büyüğe sıralama
+
+            int[] sayiDizisi = { 23, 12, 4, 86, 72, 3, 17 };
+
+            Array.Sort(sayiDizisi);
+
+            foreach (var sayi in sayiDizisi)
+            {
+                Console.WriteLine(sayi);
+            }
+            Console.WriteLine("-----------------------------------------------");
+
+            //clear
+
+            Array.Clear(sayiDizisi, 0, 2);
+
+            foreach (var sayi in sayiDizisi)
+            {
+                Console.WriteLine(sayi);
+            }
+            Console.WriteLine("-----------------------------------------------");
+
+            //Reverse - tersine çevirme
+
+            Array.Reverse(sayiDizisi);
+
+            foreach (var sayi in sayiDizisi)
+            {
+                Console.WriteLine(sayi);
+            }
+            Console.WriteLine("-----------------------------------------------");
+
+            //IndexOf
+
+            Console.WriteLine(Array.IndexOf(sayiDizisi, 23));
+
+            Console.WriteLine("-----------------------------------------------");
+
+            //Resize
+
+            Array.Resize<int>(ref sayiDizisi, 10);
+            sayiDizisi[8] = 99;
+
+            foreach (var sayi in sayiDizisi)
+            {
+                Console.WriteLine(sayi);
+            }
+        }
+    }
+}
