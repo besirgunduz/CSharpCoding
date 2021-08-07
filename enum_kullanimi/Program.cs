0d9ed5db001161222a426459a1f8052b907bcb0a
Besir Gunduz <besirgunduz1993@gmail.com>
2021-08-07 22:14:05 +0300

    Added enum_kullanimi

---
+++ enum_kullanimi/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace enum_kullanimi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Gunler.Perşembe);
+
+            int sicaklik = 36;
+
+            if (sicaklik<=(int)HavaDurumu.Normal)
+            {
+                Console.WriteLine("Havanın ısınmasını bekle");
+            }
+            else
+            {
+                Console.WriteLine("Hava sıcak...");
+            }
+
+        }
+    }
+
+    enum Gunler
+    {
+        Pazartesi,
+        Salı,
+        Çarşamba=26,
+        Perşembe,
+        Cuma,
+        Cumartesi,
+        Pazar
+    }
+
+    enum HavaDurumu
+    {
+        Soguk=0,
+        Normal=20
+    }
+}
