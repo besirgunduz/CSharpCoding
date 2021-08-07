1313aa0f790bdd4a01672a0bf339de5637ef27ad
Besir Gunduz <besirgunduz1993@gmail.com>
2021-08-07 20:52:06 +0300

    Added class_kavrami

---
+++ class_kavrami/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace class_kavrami
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Erişim Belirleyiciler
+            //Public - heryerden erişilebilir
+            //Private - sadece tanımlandığı sınıftan erişilebilir
+            //Internal - Sadece bulunduğu proje içerisinden erişilebilir
+            //Protected - Sadece tanımlandığı sınıftan ve o sınıfı miras alan sınıflardan erişilebilir.
+
+            Calisan calisan = new Calisan()
+            {
+                Id = 1,
+                Adi = "Beşir",
+                Soyad = "Gündüz"
+            };
+
+            Console.WriteLine(calisan.Adi);
+
+        }
+    }
+
+    class Calisan
+    {
+        public int Id { get; set; }
+        public string Adi { get; set; }
+        public string Soyad { get; set; }
+    }
+}
