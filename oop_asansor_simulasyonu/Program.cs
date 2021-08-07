c93a62c0eaa09b987ba8871fceba46478c8f310c
Besir Gunduz <besirgunduz1993@gmail.com>
2021-08-08 02:19:14 +0300

    Added oop_asansor_simulasyonu

---
+++ oop_asansor_simulasyonu/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace oop_asansor_simulasyonu
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
