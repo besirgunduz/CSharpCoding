4c163ec32c5fd8e0d560f2d2174af08d15960162
Besir Gunduz <besirgunduz1993@gmail.com>
2021-08-07 21:00:16 +0300

    Added constructor_kavrami

---
+++ constructor_kavrami/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace constructor_kavrami
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calisan calisan = new Calisan();
+            Console.WriteLine(calisan.Adi);
+        }
+    }
+
+    class Calisan
+    {
+        public Calisan()
+        {
+            Id = 1;
+            Adi = "Beşir";
+            Soyadi = "Gündüz";
+        }
+
+        public int Id { get; set; }
+        public string Adi { get; set; }
+        public string Soyadi { get; set; }
+    }
+}
