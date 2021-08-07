bde2db782907719ac5e9470496f3cbf8434fcbda
Besir Gunduz <besirgunduz1993@gmail.com>
2021-08-07 09:18:13 +0300

    Added arrayList

---
+++ arrayList/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace arrayList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList arrayList = new ArrayList();
+
+            arrayList.Add("Beşir");
+            arrayList.Add(true);
+
+
+            foreach (var item in arrayList)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine("---------------------------------------------------------");
+
+            //AddRange - tek seferde birden fazla eleman eklememizi sağlar.
+            string[] renkler = new string[] { "kırmızı", "sarı" };
+            List<int> sayilar = new List<int> { 1, 4, 5, 3 };
+            arrayList.AddRange(renkler);
+            arrayList.AddRange(sayilar);
+
+            foreach (var item in arrayList)
+            {
+                Console.WriteLine(item);
+            }
+
+            //Sort
+            sayilar.Sort();
+
+            //Reverse - Tersine çevirir.
+            arrayList.Reverse();
+
+            foreach (var item in arrayList)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
