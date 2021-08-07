d28bedf422aa10b58c637fdc361ce125b161817c
Besir Gunduz <besirgunduz1993@gmail.com>
2021-08-07 21:35:18 +0300

    Added static_sinif_ve_uyeler

---
+++ static_sinif_ve_uyeler/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace static_sinif_ve_uyeler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Calisan.CalisanSayisi);
+
+        }
+    }
+
+    static class Calisan
+    {
+        private static int calisanSayisi;
+        public static int CalisanSayisi { get => calisanSayisi; set => calisanSayisi = value; }
+        static Calisan()
+        {
+            calisanSayisi++;
+        }
+    }
+}
