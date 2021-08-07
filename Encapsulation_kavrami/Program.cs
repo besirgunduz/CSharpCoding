1b889c6b3987edbcf1f292f2a6018a708476b258
Besir Gunduz <besirgunduz1993@gmail.com>
2021-08-07 21:18:57 +0300

    Added Encapsulation_kavrami

---
+++ Encapsulation_kavrami/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Encapsulation_kavrami
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    class Ogrenci
+    {
+        public Ogrenci(string isim)
+        {
+            this.isim = isim;
+        }
+
+        private string isim;
+        public string Isim
+        {
+            get { return isim; }
+            set { isim = value; }
+        }
+
+    }
+}
