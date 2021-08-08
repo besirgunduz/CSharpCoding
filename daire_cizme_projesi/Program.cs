f508b2d74eb4f4f6bda5d01e708ef25602a4aa90
BESIR GUNDUZ <besirgunduz1993@gmail.com>
2021-08-08 18:34:10 +0300

    Added daire_cizme_projesi

---
+++ daire_cizme_projesi/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace daire_cizme_projesi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int yaricap;
+            double yaricap_kalinlik = 0.4;
+
+            Console.Write("Yarıçap Giriniz : ");
+            yaricap = Convert.ToInt32(Console.ReadLine());
+
+            DaireCiz(yaricap, yaricap_kalinlik);
+
+            Console.ReadKey();
+        }
+
+        static void DaireCiz(int yaricap, double yaricap_kalinlik)
+        {
+            double rIn = yaricap - yaricap_kalinlik, rOut = yaricap + yaricap_kalinlik;
+
+            for (double y = yaricap; y >= -yaricap; --y)
+            {
+                for (double x = -yaricap; x < rOut; x += 0.5)
+                {
+                    double value = x * x + y * y;
+                    if (value >= rIn * rIn && value <= rOut * rOut)
+                    {
+                        Console.Write("*");
+                    }
+                    else
+                    {
+                        Console.Write(" ");
+                    }
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
