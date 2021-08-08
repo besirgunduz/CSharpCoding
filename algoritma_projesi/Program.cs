5510efe6f176eaefd424d59a842d1bd4964f92d1
BESIR GUNDUZ <besirgunduz1993@gmail.com>
2021-08-08 18:52:27 +0300

    Added algoritma_projesi

---
+++ algoritma_projesi/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+
+namespace algoritma_projesi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Bir metin ve sayı giriniz : ");
+            string[] girdi = Console.ReadLine().Split(',');
+
+            string metin = girdi[0];
+            int sayi = Convert.ToInt32(girdi[1]);
+
+            metin = metin.Remove(sayi, 1);
+
+            Console.WriteLine(metin);
+            
+
+        }
+    }
+}
