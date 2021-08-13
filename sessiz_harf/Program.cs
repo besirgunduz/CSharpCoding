c63641856c40e2d9f9bf15117ed432be62a818d1
Besir Gunduz <besirgunduz1993@gmail.com>
2021-08-13 23:47:49 +0300

    Added sessiz_harf

---
+++ sessiz_harf/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace sessiz_harf
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Verilen string ifade içerisinde yanyana 2 tane sessiz varsa ekrana true, yoksa false yazdıran console uygulamasını yazınız.
+
+            string ifade = "Arya";
+
+            var sonuc = sessiz_harf(ifade) == true ? "true" : "false";
+
+            Console.WriteLine(sonuc);
+
+        }
+
+        private static bool sessiz_harf(string ifade)
+        {
+            var sesli = "aeıioöuü";
+
+            ifade = ifade.ToLower();
+
+            for (int i = 0; i < ifade.Length; i++)
+            {
+                if ((i + 1) < ifade.Length && !sesli.Contains(ifade[i]) && !sesli.Contains(ifade[i + 1]))
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    }
+}
