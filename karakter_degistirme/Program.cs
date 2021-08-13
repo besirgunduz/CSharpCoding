0b01b0b26a6173bcbfe704b2b4d340de3e09c034
Besir Gunduz <besirgunduz1993@gmail.com>
2021-08-13 23:16:29 +0300

    Updated karakter_degistirme

--- karakter_degistirme/Program.cs
+++ karakter_degistirme/Program.cs
@@ -8,29 +8,23 @@ namespace karakter_degistirme
         {
             //Verilen string ifade içerisindeki ilk ve son karakterin yerini değiştirip tekrar ekrana yazdıran console uygulamasını yazınız.
 
-            var ifade = "Algoritma"; // reşiB
+            var ifade = "Merhaba";
+
+            Karakter_degistirme(ifade);
+
+        }
+
+        private static void Karakter_degistirme(string ifade)
+        {
             var ilkKarakter = ifade[0];
-            var sonKarakter = ifade[ifade.Length-1];
+            var sonKarakter = ifade[ifade.Length - 1];
 
-            var sonuc = string.Empty;
+            var chars = ifade.ToCharArray();
 
-            for (int i = 0; i < ifade.Length; i++)
-            {
-                if (i == 0)
-                {
-                    sonuc += sonKarakter;
-                }
-                else if (i == ifade.Length - 1)
-                {
-                    sonuc += ilkKarakter;
-                }
-                else
-                {
-                    sonuc += ifade[i];
-                }
-            }
+            chars[0] = sonKarakter;
+            chars[ifade.Length - 1] = ilkKarakter;
 
-            Console.WriteLine(sonuc);
+            Console.WriteLine(new string(chars));
         }
     }
 }
