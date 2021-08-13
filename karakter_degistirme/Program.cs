bcb83839b444753ad99e659da4c04026fb8dc1b5
BESIR GUNDUZ <besirgunduz1993@gmail.com>
2021-08-13 18:50:41 +0300

    Added karakter_degistirme

---
+++ karakter_degistirme/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace karakter_degistirme
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Verilen string ifade içerisindeki ilk ve son karakterin yerini değiştirip tekrar ekrana yazdıran console uygulamasını yazınız.
+
+            var ifade = "Algoritma"; // reşiB
+            var ilkKarakter = ifade[0];
+            var sonKarakter = ifade[ifade.Length-1];
+
+            var sonuc = string.Empty;
+
+            for (int i = 0; i < ifade.Length; i++)
+            {
+                if (i == 0)
+                {
+                    sonuc += sonKarakter;
+                }
+                else if (i == ifade.Length - 1)
+                {
+                    sonuc += ilkKarakter;
+                }
+                else
+                {
+                    sonuc += ifade[i];
+                }
+            }
+
+            Console.WriteLine(sonuc);
+        }
+    }
+}
