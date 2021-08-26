be7199f5376b9e1341d3843d2b4f9a9b8ee69c7a
Besir Gunduz <besirgunduz1993@gmail.com>
2021-08-27 00:12:41 +0300

    added AppRepository

---
+++ BookStore/DBOperations/AppRepository.cs
@@ -0,0 +1,50 @@
+﻿using BookStore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookStore.DBOperations
+{
+    public class AppRepository : IAppRepository
+    {
+
+        BookStoreDbContext _context;
+        public AppRepository(BookStoreDbContext context)
+        {
+            _context = context;
+        }
+
+        public void Add<T>(T entity) where T : class
+        {
+            _context.Add(entity);
+        }
+
+        public void Delete<T>(T entity) where T : class
+        {
+            _context.Remove(entity);
+        }
+
+        public void Update<T>(T entity) where T : class
+        {
+            _context.Update(entity);
+        }
+
+        public bool SaveAll()
+        {
+            return _context.SaveChanges() > 0;
+        }
+
+        public List<Book> GetBooks()
+        {
+            var books = _context.Books.ToList();
+            return books;
+        }
+
+        public Book GetById(int id)
+        {
+            var book = _context.Books.Where(b => b.Id == id).SingleOrDefault();
+            return book;
+        }
+    }
+}
