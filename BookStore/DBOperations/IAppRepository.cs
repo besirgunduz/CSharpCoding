92cdd4ead3e4e0adb9b7d32563d1116d608393b5
Besir Gunduz <besirgunduz1993@gmail.com>
2021-08-27 00:13:20 +0300

    Added IAppRepository

---
+++ BookStore/DBOperations/IAppRepository.cs
@@ -0,0 +1,18 @@
+﻿using BookStore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookStore.DBOperations
+{
+    public interface IAppRepository
+    {
+        void Add<T>(T entity) where T : class;
+        void Update<T>(T entity) where T : class;
+        void Delete<T>(T entity) where T : class;
+        bool SaveAll();
+        List<Book> GetBooks();
+        Book GetById(int id);
+    }
+}
--- BookStore/Startup.cs
+++ BookStore/Startup.cs
@@ -1,7 +1,9 @@
+using BookStore.DBOperations;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -32,6 +34,11 @@ namespace BookStore
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "BookStore", Version = "v1" });
             });
+
+            services.AddDbContext<BookStoreDbContext>(x =>
+                x.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
+            services.AddScoped<IAppRepository, AppRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
