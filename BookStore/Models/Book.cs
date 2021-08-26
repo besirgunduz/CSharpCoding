13be360390a72a0cd9f56a2bc26bf86f74238b8c
Besir Gunduz <besirgunduz1993@gmail.com>
2021-08-27 00:11:26 +0300

    updated Book

--- BookStore/Models/Book.cs
+++ BookStore/Models/Book.cs
@@ -8,9 +8,8 @@ namespace BookStore.Models
     public class Book
     {
         public int Id { get; set; }
-        public string Title { get; set; }
-        public int GenreId { get; set; }
-        public int PageCount { get; set; }
-        public DateTime PublishDate { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public int AuthorId { get; set; }
     }
 }
