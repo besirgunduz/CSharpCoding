cbafd4583c300c699d25292cc5b938a1d203012c
Besir Gunduz <besirgunduz1993@gmail.com>
2021-08-29 00:28:30 +0300

    add Helpers

---
+++ BookStore/Helpers/AutoMapperProfiles.cs
@@ -0,0 +1,29 @@
+﻿using AutoMapper;
+using BookStore.Dtos;
+using BookStore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookStore.Helpers
+{
+    public class AutoMapperProfiles : Profile
+    {
+        public AutoMapperProfiles()
+        {
+            CreateMap<GetBooksDto, Book>();
+            CreateMap<Book, GetBooksDto>();
+
+            CreateMap<GetByIdDto, Book>();
+            CreateMap<Book, GetByIdDto>();
+
+            CreateMap<AddBookDto, Book>();
+            CreateMap<Book, AddBookDto>();
+
+
+
+
+        }
+    }
+}
