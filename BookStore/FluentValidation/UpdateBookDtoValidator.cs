0541d0fa7c6547e191e0618e7a09a23c0504cc81
Besir Gunduz <besirgunduz1993@gmail.com>
2021-08-29 23:05:25 +0300

    update

---
+++ BookStore/FluentValidation/UpdateBookDtoValidator.cs
@@ -0,0 +1,17 @@
+﻿using BookStore.Dtos;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookStore.FluentValidation
+{
+    public class UpdateBookDtoValidator : AbstractValidator<UpdateBookDto>
+    {
+        public UpdateBookDtoValidator()
+        {
+            RuleFor(b => b.Name).MinimumLength(5);
+        }
+    }
+}
