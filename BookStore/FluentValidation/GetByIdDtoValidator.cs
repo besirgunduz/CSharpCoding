0bb3911029e21d4054ace5c52e637a84229b2312
Besir Gunduz <besirgunduz1993@gmail.com>
2021-08-29 23:05:05 +0300

    added GetByIdDtoValidator

---
+++ BookStore/FluentValidation/GetByIdDtoValidator.cs
@@ -0,0 +1,17 @@
+﻿using BookStore.Dtos;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookStore.FluentValidation
+{
+    public class GetByIdDtoValidator: AbstractValidator<GetByIdDto>
+    {
+        public GetByIdDtoValidator()
+        {
+            RuleFor(b => b.Id).GreaterThan(0);
+        }
+    }
+}
