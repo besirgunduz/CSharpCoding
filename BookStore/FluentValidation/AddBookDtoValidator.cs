f4d5154f99d90a4870734953ed979eff25a7e2f2
Besir Gunduz <besirgunduz1993@gmail.com>
2021-08-29 23:04:52 +0300

    added AddBookDtoValidator

---
+++ BookStore/FluentValidation/AddBookDtoValidator.cs
@@ -0,0 +1,17 @@
+﻿using BookStore.Dtos;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookStore.FluentValidation
+{
+    public class AddBookDtoValidator:AbstractValidator<AddBookDto>
+    {
+        public AddBookDtoValidator()
+        {
+            RuleFor(b => b.Name).NotEmpty().MinimumLength(4);
+        }
+    }
+}
