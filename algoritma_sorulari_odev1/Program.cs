c446409e6b8205bd41e42ebf4fcfa77f58fef72e
Besir Gunduz <besirgunduz1993@gmail.com>
2021-11-10 15:50:04 +0300

    updated

--- algoritma_sorulari_odev1/Program.cs
+++ algoritma_sorulari_odev1/Program.cs
@@ -8,22 +8,29 @@ namespace algoritma_sorulari_odev1
         {
 
             // 1.çözüm
-
-
-            Console.Write("Pozitif bir tam sayı giriniz : ");
-            int sayi = Convert.ToInt32(Console.ReadLine());
-
-            Console.Write($"{sayi} adet pozitif tam sayı giriniz : ");
-
-            var sayilar = Console.ReadLine().Split(' ');
-
-            foreach (var item in sayilar)
+            try
             {
-                if (Convert.ToInt32(item) % 2 == 0)
+                Console.Write("Pozitif bir tam sayı giriniz : ");
+                int sayi = Convert.ToInt32(Console.ReadLine());
+
+                Console.Write($"{sayi} adet pozitif tam sayı giriniz : ");
+
+                var sayilar = Console.ReadLine().Split(' ');
+
+                foreach (var item in sayilar)
                 {
-                    Console.WriteLine(item);
+                    if (Convert.ToInt32(item) % 2 == 0)
+                    {
+                        Console.WriteLine(item);
+                    }
                 }
             }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Hata mesajı : {ex.Message}");
+            }
+
+
 
             Console.WriteLine("----------------------------------------------------------------------------");
 
@@ -31,20 +38,27 @@ namespace algoritma_sorulari_odev1
             // 2. çözüm
 
 
-            Console.Write("Pozitif iki tam sayı giriniz : ");
-            var sayilar2 = Console.ReadLine().Split(' ');
-            int nSayisi = Convert.ToInt32(sayilar2[0]);
-            int mSayisi = Convert.ToInt32(sayilar2[1]);
-
-            Console.Write($"{nSayisi} adet pozitif sayı giriniz : ");
-            var sayilar3 = Console.ReadLine().Split(' ');
-
-            foreach (var item in sayilar3)
+            try
             {
-                if (Convert.ToInt32(item) == mSayisi || Convert.ToInt32(item) % 2 == 0)
+                Console.Write("Pozitif iki tam sayı giriniz : ");
+                var sayilar2 = Console.ReadLine().Split(' ');
+                int nSayisi = Convert.ToInt32(sayilar2[0]);
+                int mSayisi = Convert.ToInt32(sayilar2[1]);
+
+                Console.Write($"{nSayisi} adet pozitif sayı giriniz : ");
+                var sayilar3 = Console.ReadLine().Split(' ');
+
+                foreach (var item in sayilar3)
                 {
-                    Console.WriteLine(item);
+                    if (Convert.ToInt32(item) == mSayisi || Convert.ToInt32(item) % mSayisi == 0)
+                    {
+                        Console.WriteLine(item);
+                    }
                 }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Hata mesajı : {ex.Message}");
             }
 
             Console.WriteLine("----------------------------------------------------------------------------");
@@ -52,16 +66,24 @@ namespace algoritma_sorulari_odev1
 
             //3. çözüm
 
-
-            Console.Write("Pozitif bir tam sayı giriniz : ");
-            int PozitifSayi = Convert.ToInt32(Console.ReadLine());
-
-            Console.Write($"{PozitifSayi} adet kelime giriniz : ");
-            var kelimeler = Console.ReadLine().Split(' ');
-
-            for (int i = kelimeler.Length - 1; i >= 0; i--)
+            try
             {
-                Console.WriteLine(kelimeler[i]);
+
+                Console.Write("Pozitif bir tam sayı giriniz : ");
+                int PozitifSayi = Convert.ToInt32(Console.ReadLine());
+
+                Console.Write($"{PozitifSayi} adet kelime giriniz : ");
+                var kelimeler = Console.ReadLine().Split(' ');
+
+                for (int i = kelimeler.Length - 1; i >= 0; i--)
+                {
+                    Console.WriteLine(kelimeler[i]);
+                }
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine($"Hata mesajı : {ex.Message}");
             }
 
             Console.WriteLine("----------------------------------------------------------------------------");
@@ -70,17 +92,33 @@ namespace algoritma_sorulari_odev1
             //4. çözüm
 
 
-            Console.Write("Bir cümle yazınız : ");
-            var cumle = Console.ReadLine().Split(' ');
-            Console.WriteLine("Toplam kelime sayısı : "+cumle.Length);
-
-            int harfSayisi = 0;
-            for (int i = 0; i < cumle.Length; i++)
+            try
             {
-                harfSayisi += cumle[i].Length;
-            }
+                Console.Write("Bir cümle yazınız : ");
+                var cumle = Console.ReadLine();
 
-            Console.WriteLine("Toplam harf sayısı : " + harfSayisi);
+                var kelimeSayisi = 0;
+                var harfSayisi = 0;
+
+                foreach (var kelime in cumle.Split())
+                {
+                    if (kelime != "")
+                        kelimeSayisi++;
+                }
+
+                for (int i = 0; i < cumle.Length; i++)
+                {
+                    if (char.IsLetter(cumle[i]))
+                        harfSayisi++;
+                }
+
+                Console.WriteLine("Toplam kelime sayısı : " + kelimeSayisi);
+                Console.WriteLine("Toplam harf sayısı : " + harfSayisi);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Hata mesajı : {ex.Message}");
+            }
 
 
         }
