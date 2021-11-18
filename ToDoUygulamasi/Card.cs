f7d72828389d5543505effb88e71c7203a1db866
Besir Gunduz <besirgunduz1993@gmail.com>
2021-11-18 23:27:36 +0300

    added Card

---
+++ ToDoUygulamasi/Card.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDoUygulamasi
+{
+    public class Card
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Contant { get; set; }
+        public int? AssignedPersonId { get; set; }
+        public Size Size { get; set; }
+        public virtual AssignedPerson AssignedPerson { get; set; }
+    }
+}
