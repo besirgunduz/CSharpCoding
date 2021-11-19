723ed66cdce7125a288d2733e4179ecf354340d9
Besir Gunduz <besirgunduz1993@gmail.com>
2021-11-19 23:32:16 +0300

    updated AssignedPerson

---
+++ ToDoUygulamasi/AssignedPerson.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDoUygulamasi
+{
+    public class AssignedPerson
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        //public virtual ICollection<Card> Cards { get; set; }
+    }
+}
