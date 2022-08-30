2793329cf1b812ca54e584880fe879e7602d7cd5
Besir Gunduz <besirgunduz1993@gmail.com>
2022-08-30 12:17:46 +0300

    StringvsStringBuilder

---
+++ StringvsStringBuilder/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Diagnostics;
+using System.Text;
+
+namespace StringvsStringBuilder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int counter = 100000;
+            Stopwatch sw = Stopwatch.StartNew();
+
+
+            StringBuilder sb = new StringBuilder();
+            for (int i = 1; i < counter; i++)
+            {
+                sb.Append(i.ToString());
+            }
+
+            sw.Stop();
+            Console.WriteLine("StringBuilder : " + sw.ElapsedMilliseconds);
+
+
+            sw.Restart();
+            string test = "";
+            for (int i = 1; i < counter; i++)
+            {
+                test += i.ToString();
+            }
+
+            sw.Stop();
+            Console.WriteLine("String : " + sw.ElapsedMilliseconds);
+        }
+    }
+}
